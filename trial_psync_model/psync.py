114701cb112351a85239b7263de94e91836e90c9
wrijupan <wriju@wrijupans-air.speedport_w_723v_1_43_000>
2017-05-27 12:55:16 +0200

    Reverted back to 'Synchrotron' as the parent class of 'PSynchrotron'

--- trial_psync_model/psync.py
+++ trial_psync_model/psync.py
@@ -1,4 +1,4 @@
-from naima.models import Synchrotron, PionDecay
+from naima.models import Synchrotron
 from naima.models import ExponentialCutoffPowerLaw as ECPL
 from naima.models import ExponentialCutoffBrokenPowerLaw as EBPL
 from naima.models import PowerLaw as PL
@@ -11,12 +11,13 @@ import numpy as np
 from scipy.special import cbrt
 import matplotlib.pyplot as plt
 
+__all__ = ['PSynchrotron']
 
 mpc2 = (m_p * c ** 2).cgs
 mpc2_unit = u.Unit(mpc2)
 e = e.gauss
 
-class PSynchrotron(Synchrotron, PionDecay):
+class PSynchrotron(Synchrotron):
     """Dummy Proton-Synchrotron Model
        Simplest modification of Naima e-synchrotron code.
        Compute synchrotron for random magnetic field according to approximation
@@ -32,16 +33,23 @@ class PSynchrotron(Synchrotron, PionDecay):
                         physical_type='differential energy')
         self.Epmin = 1e12 * u.eV
         self.Epmax = 4e20 * u.eV
-        self.nEpd = 200
+        self.nEpd = 300
         self.__dict__.update(**kwargs)
 
 
-
+    @property
+    def _energyarr(self):
+        """Proton energy array
+        """
+        return np.logspace(np.log10(self.Epmin.to(u.eV).value),
+                           np.log10(self.Epmax.to(u.eV).value),
+                           self.nEpd * (np.log10(self.Epmax / self.Epmin)))
+        
     @property
     def _lorentzfacarr(self):
         """Lorentz factor of protons
         """
-        return self._Ep / mpc2.to(u.GeV).value
+        return self._energyarr / mpc2.to(u.eV).value
 
 
     @property
@@ -109,12 +117,12 @@ if __name__ == '__main__':
         sed = pd.sed(specen, dist)
         ax.loglog(specen, sed, lw=2,
                 color=cs, ls=ls, label=lb)
-    ax.set_xlabel('Energy (eV)', fontsize=14)
+    ax.set_xlabel('Energy (eV)', fontsize=17)
     ax.set_ylabel(
         r'$E^{2}*{\rm d}N/{\rm d}E\,[erg\,cm^{-2}\,s^{-1}]$', fontsize=17)
     ax.set_title('Proton Synchrotron Model', fontsize=17)
     ax.set_ylim(1e-21, 1e-3)
     ax.set_xlim(1e0, 1e14)
-    plt.legend(loc='best')
+    plt.legend(loc='best', borderpad=2, fontsize=14)
     plt.savefig('psync_model_comparison.eps')
     plt.show()
