9853f2c82f5f41d0979cf1143270c91fd05145d8
wrijupan <wriju@s68dyn114.zeuthen.desy.de>
2017-07-13 12:24:09 +0200

    Added optical depth calc for PowerLaw target photons

--- cascade_rad/calc_opt_depth.py
+++ cascade_rad/calc_opt_depth.py
@@ -1,23 +1,43 @@
+########################################################################
+# Module for optical depth calculation
+# TauCalcPWL : Calculates optical depth for PowerLaw target photon field
+# TauCalcBB : Calculates optical depth for Blackbody target photon field
+
+# References :
+# 1) 'HE Radiation from BH' (authors : Dermer, C. D. et al.)
+# 2) 'https://arxiv.org/pdf/1706.07047v1.pdf'
+
+# 13 July 2017
+# Authors : Cosimo Nigro (cosimo.nigro@desy.de),
+#           Wrijupan Bhayyacharyya (wrijupan.bhattacharyya@desy.de)
+#########################################################################
+from __future__ import division
 from astropy import units as u
 from astropy.constants import m_e, k_B, sigma_T, c, hbar
 from scipy.integrate import quad
 import numpy as np
 from numpy import log, sqrt
 import matplotlib.pyplot as plt
+from math import log, sqrt, pi
+import astropy.constants as const
+from astropy.cosmology import FlatLambdaCDM
+from sympy.functions.special.delta_functions import Heaviside
+import matplotlib.pyplot as plt
 
 _sigma_T = sigma_T.cgs.value
 _mec2 = (m_e * c ** 2).to('eV').value
 _mec2_u = (m_e * c ** 2).to('eV')
+# we declare our cosmology for later caluclations
+cosmo = FlatLambdaCDM(H0=70 * u.km / u.s / u.Mpc, Tcmb0=2.725 * u.K, Om0=0.3)
 
-__all__ = ['EMCascade']
+__all__ = ['CalcOptDepthBB', 'CalcOptDepthPWL', ]
 
-class EMCascade(object):
+
+class CalcOptDepthBB(object):
     """
-    Class to calculate electromagnetic cascade radiation.
-    Incomplete at present. At present only a module to calculate
-    optical depth as a func of gamma-ray energy.
-    Soft photon field is only a BlackBody at present, whose
-    temperature is an input parameter of EMC class.
+    Module to calculate optical depth as a func of gamma-ray energy
+    for BlackBody Soft Photon Field, whose temperature is an input
+    parameter of TauCalcBB class.
 
     'calc_opt_depth' : method to be called for optical depth calc.
 
@@ -38,6 +58,7 @@ class EMCascade(object):
     4) Leave the normalizaion of soft ph dist as free parameter?
        (at present fixed)
     """
+
     def __init__(self, bb_temp, size):
         """
         Parameters
@@ -72,13 +93,6 @@ class EMCascade(object):
             soft photon energy (normalized)
         E = float
             Gamma-ray energy (normalized)
-
-        Reference
-        ---------
-        'High Energy Radiation From Black Holes'
-            (Authors : Charles D. Dermer, Govind Menon)
-        Condensed form : Cosimo's presentation
-            '2-17-06-27 LITERATURE Meeting Notes'
         """
         s = e * E
         beta = sqrt(1 - (1. / s))
@@ -105,8 +119,118 @@ class EMCascade(object):
         return tau
 
 
+class CalcOptDepthPWL(object):
+    """
+    Module to calculate optical depth as a func of gamma-ray energy
+    for Broken PWL target Photon Field.
+
+    'tau_YY' : Method to be called for optical depth calculation
+    """
+
+    def __init__(self):
+        pass
+
+    def phi_bar(self, s_0):
+        """
+        Implementing Eq. (10.9) of Ref.1
+        """
+        if s_0 > 1:
+
+            beta_0 = sqrt(1 - 1 / s_0)
+            w_0 = (1 + beta_0) / (1 - beta_0)
+
+            def _L(w):
+                """
+                Integrand of Eq. (10.10)
+                """
+                return 1 / w * log(1 + w)
+
+            # first line of Eq. (10.9)
+            term1 = (1 + beta_0 ** 2) / (1 - beta_0 ** 2) * log(w_0) - beta_0 ** 2 * log(w_0) - \
+                log(w_0) ** 2 - (4 * beta_0) / (1 - beta_0 ** 2)
+            # second line of Eq. (10.9)
+            term2 = 2 * beta_0 + 4 * \
+                log(w_0) * log(1 + w_0) - 4 * quad(_L, 1., w_0)[0]
+
+            return term1 + term2
+
+        else:
+            return 0
+
+    def plot_phi_bar(self):
+        """
+        simple function for plotting phi_ba and checking it with Figure 10.2 of ref.
+        """
+        s_0 = np.linspace(1.1, 10, 1e2)
+        _phi = np.array([phi_bar(s) for s in s_0])
+
+        plt.plot(s_0, _phi / (s_0 - 1), lw=1.5)
+        plt.xlabel(r'$s_0$', fontsize=14)
+        plt.ylabel(r'$\frac{\overline{\phi}(s_0)}{s_0 - 1}$', fontsize=18)
+        plt.ylim([0, 5.])
+        plt.show()
+
+    def tau_YY(self, E, z, eps_pk, f_eps_pk, t_var, delta_D, a, b, x_a=1e-4, x_b=1e4):
+        """
+        Function for evaluating the optical depth as a function of the energy
+        in the case of target photon gas with broken power-law photon distribution
+        Eq. (10.27) of reference 1.
+
+        Parameters:
+        -----------
+        E : astropy.units('eV')
+            energy of the colliding photon
+        z : float
+            redshift of the source
+        eps_pk : float
+            adimensional energy peak of the target photon distribution
+        f_eps_pk : astropy.units('erg cm-2 s-1')
+            peak of the target photon distribution
+        t_var : astropy.units('s')
+            measured variability time scale
+        delta_D : float
+            relativistic doppler factor
+        a : float
+            first spectral index of the broken power-law describing the target
+        b : float
+            second spectral index of the broken power-law describing the target
+
+        Returns:
+        --------
+        float, value of the tau_YY for an incident photon of energy E
+        """
+
+        # listed all the values we need for the prefactor calculation
+        r_e = 2.8179403227 * 1e-13 * u.cm
+        # we have to get the luminosity distance from the redshift
+        # we use astropy cosmology
+        d_L = cosmo.luminosity_distance(z)
+        # dimensionless energy of the colliding photon
+        eps_1 = (E / (const.m_e * const.c ** 2)).decompose().value
+        # this is the multiplicative factor of the integral in the second line
+        # in Eq. (10.27)
+        prefactor_num = 3 * pi * r_e ** 2 * d_L ** 2 * f_eps_pk
+        prefactor_denom = const.m_e * const.c ** 4 * \
+            t_var * eps_1 ** 2 * eps_pk ** 3 * (1 + z) ** 4
+        prefactor = (prefactor_num / prefactor_denom).decompose().value
+
+        # the integral
+        w = (1 + z) ** 2 * eps_1 * eps_pk / delta_D ** 2
+
+        def integrand1(x): return self.phi_bar(x * w) / (x ** (4 - a))
+        integral1 = Heaviside(1 - 1 / w) * \
+            quad(integrand1, max(x_a, 1 / w), 1)[0]
+
+        def integrand2(x): return self.phi_bar(x * w) / (x ** (4 - b))
+        integral2 = quad(integrand2, max(1, 1 / w), x_b)[0]
+
+        return prefactor * (integral1 + integral2)
+
+
 if __name__ == '__main__':
-    Tarr = [500, 750, 1000] * u.K
+
+    ################## Test for Class CalcOptDepthBB #############
+    Tarr = [500, 1000] * u.K
     s = 1 * u.kpc
 
     Emin = 4e2 * u.GeV
@@ -115,15 +239,15 @@ if __name__ == '__main__':
 
     tau_dict = {}
 
-    for i, T in enumerate(Tarr) :
+    for i, T in enumerate(Tarr):
         tau_dict[T] = []
-        emc = EMCascade(T, s)
+        calcdepth = CalcOptDepthBB(T, s)
         for E in Earr:
             E = E.to('eV') / _mec2_u
-            tau = emc.calc_opt_depth(E)
+            tau = calcdepth.calc_opt_depth(E)
             tau_dict[T].append(np.exp(-tau))
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
+    fig1 = plt.figure()
+    ax = fig1.add_subplot(1, 1, 1)
     xticks = Earr.value
     ax.set_xticks(xticks, minor=True)
     ax.grid(which='both', alpha=0.2)
@@ -134,10 +258,25 @@ if __name__ == '__main__':
     plt.xlabel(r'$E_\gamma$ [TeV]')
     plt.ylabel(r'$exp(- \tau_{\gamma\gamma})$')
     plt.legend(loc='best')
-    plt.savefig('./images/exptau_comparison.png')
+    fig1.savefig('./images/exptau_comparison.png')
     plt.show()
 
+    ################## Test for Class CalcOptDepthPWL #############
+    taupl = CalcOptDepthPWL()
+    taupl.plot_phi_bar
 
+    En = np.logspace(9, 16, 50) * u.Unit('eV')
 
+    fig2 = plt.figure()
 
+    for [a, b] in [[1 / 2, -1 / 2], [1, -1], [2, -2]]:
+        tau = np.array([taupl.tau_YY(
+            _E, 1, 1e-5, 1e-10 * u.Unit('erg cm-2 s-1'), 1e5 * u.s, 10, a, b) for _E in En])
+        plt.loglog(En, tau, lw=1.5, label='a=' + str(a) + ' , b=' + str(b))
 
+    plt.legend(loc=2)
+    plt.xlabel('E[eV]')
+    plt.ylabel(r'$\tau_{\gamma \gamma}(E)$')
+    plt.ylim([1e-2, 1e6])
+    plt.show()
+    fig2.savefig('./images/tau_PWL_trials.png')
